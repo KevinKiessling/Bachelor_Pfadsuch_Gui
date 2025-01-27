fe82da4f5980c103148bc23fb7c1b0e87e19bf04
Kevin <kiessling.kevin95@gmail.com>
2025-01-27 20:13:27 +0100

    refactor normal dijkstra draw graph into its own class

--- gui/pfadsuch_app.py
+++ gui/pfadsuch_app.py
@@ -7,6 +7,7 @@ from algorithmen.dijkstra_list import *
 from algorithmen.dijkstra_Priority_queue_lazy import *
 from algorithmen.dijkstra_Priority_queue import *
 from graph_visualizer.graph_visualizer_dijkstra_lazy import *
+from graph_visualizer.graph_visualizer_dijkstra import *
 import math
 import copy
 class PfadsuchApp(Tk):
@@ -326,13 +327,14 @@ class PfadsuchApp(Tk):
     def update_gui(self):
 
         self.graph_draw_lazy = Graph_Visualizer_Dijkstra_lazy(self.gui_frame, self.node_positions, self.graph, self.selected_nodes, self.start_node, self)
+        self.graph_draw_normal = Graph_Visualizer_Dijkstra(self.gui_frame, self.node_positions, self.graph, self.selected_nodes, self.start_node, self)
         self.gui_frame.canvas.delete("all")
 
         if self.current_step == -1:
             if self.selected_algorithm == "Dijkstra_PQ_lazy":
                 self.graph_draw_lazy.draw_graph_dijkstra_lazy(None, None, {node: 0 for node in self.graph}, set(), set())
             if self.selected_algorithm == "Dijkstra_PQ":
-                self.draw_graph_dijkstra_normal(None, None, {node: 0 for node in self.graph}, set(), set())
+                self.graph_draw_normal.draw_graph_dijkstra(None, None, {node: 0 for node in self.graph}, set(), set())
             if self.selected_algorithm == "Dijkstra_List":
                 self.draw_graph_dijkstra_list(None, None, {node: 0 for node in self.graph}, set(), set())
             if self.steps_finished_algorithm:
@@ -362,17 +364,7 @@ class PfadsuchApp(Tk):
             self.graph_draw_lazy.draw_graph_dijkstra_lazy(current_node, neighbor, distances, visited, visited_edges)
 
         if self.selected_algorithm == "Dijkstra_PQ":
-            if step["step_type"] == "Algorithm Finished":
-                self.draw_graph_dijkstra_normal(None, None, distances, visited, visited_edges)
-
-                self.code_frame.update_priority_queue(priority_queue)
-                if self.debug:
-                    print(distances)
-                if self.shortest_paths:
-                    self.gui_frame.shortest_paths_button.config(state=NORMAL)
-                return
-            else:
-                self.draw_graph_dijkstra_normal(current_node, neighbor, distances, visited, visited_edges)
+            self.graph_draw_normal.draw_graph_dijkstra(current_node, neighbor, distances, visited, visited_edges)
 
         if self.selected_algorithm == "Dijkstra_List":
             if step["step_type"] == "Algorithm Finished":
@@ -401,218 +393,6 @@ class PfadsuchApp(Tk):
                 self.gui_frame.shortest_paths_button.config(state=NORMAL)
 
 
-    #zeichnet den Graph
-    def draw_graph_dijkstra_normal(self, current_node, neighbor_list, distances, visited, visited_edges, highlight_only_edge=False):
-        self.gui_frame.canvas.delete("all")
-        node_radius = 30
-        font_size = 14
-        already_drawn_edges = set()
-        print("normal graph")
-        #basic draw node
-        for node, (x, y) in self.node_positions.items():
-            color = "lightblue"
-            if node == current_node:
-                color = self.current_node_color
-            elif node in visited:
-
-                color = self.visited_node_color
-
-            distance_text = distances.get(node, float('inf'))
-            distance_text = f"{distance_text if distance_text < float('inf') else '∞'}"
-
-            distance_length = len(distance_text)
-
-            node_length = len(node)
-            padding = max(0, distance_length - node_length)
-
-            if distances.get(node, float('inf'))>=9999:
-                font_size = 12
-                left_padding = (padding + 2) // 2
-                right_padding = padding // 2
-            else:
-                font_size = 14
-                left_padding = (padding + 1) // 2  #
-                right_padding = padding // 2
-
-            node_text = f"{' ' * left_padding}{node}{' ' * right_padding}"
-            display_text = f"{node_text}\n{distance_text}"
-
-            if node in self.selected_nodes:
-                self.gui_frame.canvas.create_oval(x - node_radius, y - node_radius, x + node_radius, y + node_radius, fill="green")
-
-                self.gui_frame.canvas.create_text(
-                    x, y, text=display_text, fill="black", font=("Arial", font_size), anchor="center"
-                )
-                if node == self.start_node:
-                    self.gui_frame.canvas.create_text(x, y, text="Start", fill="black", font=("Arial", font_size))
-                else:
-                    self.gui_frame.canvas.create_text(
-                        x, y, text=display_text, fill="black", font=("Arial", font_size), anchor="center"
-                    )
-            else:
-                self.gui_frame.canvas.create_oval(x - node_radius, y - node_radius, x + node_radius, y + node_radius, fill=color)
-
-
-                if node == self.start_node:
-                    self.gui_frame.canvas.create_text(x, y, text="Start", fill="black", font=("Arial", font_size))
-                else:
-                    self.gui_frame.canvas.create_text(x, y, text=display_text, fill="black", font=("Arial", font_size))
-
-
-
-        # Zeichne alle Kanten, dabei wird zwischen 2 Varianten unterschieden, Direkt oder Bidirekt
-        for node, edges in self.graph.items():
-            for neighbor, weight in edges.items():
-
-                if (node, neighbor) in already_drawn_edges or (neighbor, node) in already_drawn_edges:
-                    continue
-
-                edge_color = "black"
-
-                if (node, neighbor) in visited_edges or (neighbor, node) in visited_edges:
-                    edge_color = self.visited_edge_color
-                if node == current_node and neighbor == neighbor_list:
-                    edge_color = self.highlighted_edge_color
-
-                if neighbor in self.node_positions:
-                    x1, y1 = self.node_positions[node]
-                    x2, y2 = self.node_positions[neighbor]
-
-                    dx = x2 - x1
-                    dy = y2 - y1
-                    distance = math.sqrt(dx ** 2 + dy ** 2)
-
-                    if distance > 0:
-                        x1_no_node_clip = x1 + dx / distance * node_radius
-                        y1_no_node_clip = y1 + dy / distance * node_radius
-                        x2_no_node_clip = x2 - dx / distance * node_radius
-                        y2_no_node_clip = y2 - dy / distance * node_radius
-                    else:
-                        x1_no_node_clip, y1_no_node_clip, x2_no_node_clip, y2_no_node_clip = x1, y1, x2, y2
-
-                    middle_space = 0.12
-
-                    segment_dx = dx / distance * middle_space * distance
-                    segment_dy = dy / distance * middle_space * distance
-
-                    is_bidirectional = neighbor in self.graph and node in self.graph[neighbor]
-
-                    if is_bidirectional:
-                        forward_colour = "black"
-                        reverse_colour = "black"
-
-                        if (node, neighbor) in visited_edges:
-                            forward_colour = self.visited_edge_color
-                        if (neighbor, node) in visited_edges:
-                            reverse_colour = self.visited_edge_color
-                        if (node == current_node and neighbor == neighbor_list):
-                            forward_colour = self.highlighted_edge_color
-                        if (neighbor == current_node and node == neighbor_list):
-                            reverse_colour = self.highlighted_edge_color
-
-                        offset = 13
-
-                        perp_dx = -dy / distance * offset
-                        perp_dy = dx / distance * offset
-
-                        weight_text_forward = str(weight)
-                        weight_text_reverse = str(self.graph[neighbor][node])
-                        text_width_forward = len(weight_text_forward) * 6
-                        text_width_reverse = len(weight_text_reverse) * 6
-
-                        max_text_width = max(text_width_forward, text_width_reverse)
-
-                        middle_space_expanded = middle_space + max_text_width / distance * 1.5
-
-                        segment_dx_expanded = dx / distance * middle_space_expanded * distance
-                        segment_dy_expanded = dy / distance * middle_space_expanded * distance
-
-
-                        x1_offset = x1_no_node_clip + perp_dx
-                        y1_offset = y1_no_node_clip + perp_dy
-                        x2_offset = x2_no_node_clip + perp_dx
-                        y2_offset = y2_no_node_clip + perp_dy
-                        middle_x_forward = (x1_offset + x2_offset) / 2 + segment_dx_expanded / 2
-                        middle_y_forward = (y1_offset + y2_offset) / 2 + segment_dy_expanded / 2
-
-                        self.gui_frame.canvas.create_line(
-                            x1_offset, y1_offset, middle_x_forward - segment_dx_expanded / 2,
-                                                  middle_y_forward - segment_dy_expanded / 2,
-                            width=4, tags="edge", fill=forward_colour, smooth=True, splinesteps=500
-                        )
-                        self.gui_frame.canvas.create_line(
-                            middle_x_forward + segment_dx_expanded / 2, middle_y_forward + segment_dy_expanded / 2,
-                            x2_offset, y2_offset,
-                            width=4, tags="edge", arrow="last", arrowshape=(10, 12, 5), fill=forward_colour,
-                            smooth=True, splinesteps=500
-                        )
-
-                        self.gui_frame.canvas.create_text(
-                            middle_x_forward, middle_y_forward,
-                            text=weight_text_forward, fill="black", font=("Arial", 13), tags="weight"
-                        )
-
-                        # Reverse edge
-                        x1_offset = x1_no_node_clip - perp_dx
-                        y1_offset = y1_no_node_clip - perp_dy
-                        x2_offset = x2_no_node_clip - perp_dx
-                        y2_offset = y2_no_node_clip - perp_dy
-                        middle_x_reverse = (x1_offset + x2_offset) / 2 - segment_dx_expanded / 2
-                        middle_y_reverse = (y1_offset + y2_offset) / 2 - segment_dy_expanded / 2
-
-                        self.gui_frame.canvas.create_line(
-                            x2_offset, y2_offset, middle_x_reverse + segment_dx_expanded / 2,
-                                                  middle_y_reverse + segment_dy_expanded / 2,
-                            width=4, tags="edge", fill=reverse_colour, smooth=True, splinesteps=500
-                        )
-                        self.gui_frame.canvas.create_line(
-                            middle_x_reverse - segment_dx_expanded / 2, middle_y_reverse - segment_dy_expanded / 2,
-                            x1_offset, y1_offset,
-                            width=4, tags="edge", arrow="last", arrowshape=(10, 12, 5), fill=reverse_colour,
-                            smooth=True, splinesteps=500
-                        )
-
-                        self.gui_frame.canvas.create_text(
-                            middle_x_reverse, middle_y_reverse,
-                            text=weight_text_reverse, font=("Arial", 13), tags="weight"
-                        )
-
-                    else:
-                        weight_text = str(weight)
-                        text_width = len(weight_text) * 8
-
-                        middle_space += text_width / distance * 1.5
-
-                        segment_dx = dx / distance * middle_space * distance
-                        segment_dy = dy / distance * middle_space * distance
-
-                        middle_x = (x1_no_node_clip + x2_no_node_clip) / 2
-                        middle_y = (y1_no_node_clip + y2_no_node_clip) / 2
-
-                        available_space = distance * middle_space
-                        if text_width > available_space:
-                            extra_space = (text_width - available_space) / 2
-                            middle_x -= extra_space
-
-                        self.gui_frame.canvas.create_line(
-                            x1_no_node_clip, y1_no_node_clip, middle_x - segment_dx / 2, middle_y - segment_dy / 2,
-                            width=4, tags="edge", fill=edge_color, smooth=True, splinesteps=500
-                        )
-                        self.gui_frame.canvas.create_line(
-                            middle_x + segment_dx / 2, middle_y + segment_dy / 2, x2_no_node_clip, y2_no_node_clip,
-                            width=4, tags="edge", arrow="last", arrowshape=(10, 12, 5), fill=edge_color, smooth=True,
-                            splinesteps=500
-                        )
-
-                        self.gui_frame.canvas.create_text(
-                            middle_x, middle_y,
-                            text=weight_text,
-                            fill="black",
-                            font=("Arial", 14),
-                            tags="weight"
-                        )
-
-                        already_drawn_edges.add((node, neighbor))
 
     # zeichnet graph mit highlighted path zu übergebenem Endknoten
     def draw_graph_path(self, path):
